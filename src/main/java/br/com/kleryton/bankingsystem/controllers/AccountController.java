2967d574379c36839350d36e7c603f2509518f07
Kleryton <kleryton.dev@gmail.com>
2022-03-25 19:24:04 -0300

    update AccountController

--- src/main/java/br/com/kleryton/bankingsystem/controllers/AccountController.java
+++ src/main/java/br/com/kleryton/bankingsystem/controllers/AccountController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.kleryton.bankingsystem.models.AccountModel;
